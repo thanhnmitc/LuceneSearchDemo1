7364bf98a13285020b8e3fe972441c19309ef8cf
Thanh Nguyen <thanhnm.itc@hotmail.com>
2018-04-15 23:22:56 +0700

    first commit

---
+++ LuceneSearch.cs
@@ -0,0 +1,118 @@
+﻿using Lucene.Net.Index;
+using Lucene.Net.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net.Search;
+using Lucene.Net.Documents;
+using Lucene.Net.Analysis.Standard;
+
+namespace Demo1
+{
+    public class LuceneSearchEngine
+    {
+        private static string _luceneDir = Path.Combine(AppContext.BaseDirectory , "lucene_index");
+        private static FSDirectory _directoryTemp;
+        private static FSDirectory _directory
+        {
+            get
+            {
+                if (_directoryTemp == null) _directoryTemp = FSDirectory.Open(new DirectoryInfo(_luceneDir));
+                if (IndexWriter.IsLocked(_directoryTemp)) IndexWriter.Unlock(_directoryTemp);
+                var lockFilePath = Path.Combine(_luceneDir, "write.lock");
+                if (File.Exists(lockFilePath)) File.Delete(lockFilePath);
+                return _directoryTemp;
+            }
+        }
+
+        private static void _addToLuceneIndex(SampleData sampleData, IndexWriter writer)
+        {
+            // remove older index entry
+            var searchQuery = new TermQuery(new Term("Id", sampleData.Id.ToString()));
+            writer.DeleteDocuments(searchQuery);
+
+            // add new index entry
+            var doc = new Document();
+
+            // add lucene fields mapped to db fields
+            doc.Add(new Field("Id", sampleData.Id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            doc.Add(new Field("Name", sampleData.Name, Field.Store.YES, Field.Index.ANALYZED));
+            doc.Add(new Field("Description", sampleData.Description, Field.Store.YES, Field.Index.ANALYZED));
+
+            // add entry to index
+            writer.AddDocument(doc);
+        }
+
+        public static void AddUpdateLuceneIndex(IEnumerable<SampleData> sampleDatas)
+        {
+            // init lucene
+            var analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+            using (var writer = new IndexWriter(_directory, analyzer, IndexWriter.MaxFieldLength.UNLIMITED))
+            {
+                // add data to lucene search index (replaces older entry if any)
+                foreach (var sampleData in sampleDatas) _addToLuceneIndex(sampleData, writer);
+
+                // close handles
+                analyzer.Close();
+                writer.Dispose();
+            }
+        }
+
+        public static void AddUpdateLuceneIndex(SampleData sampleData)
+        {
+            AddUpdateLuceneIndex(new List<SampleData> { sampleData });
+        }
+
+        public static void ClearLuceneIndexRecord(int record_id)
+        {
+            // init lucene
+            var analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+            using (var writer = new IndexWriter(_directory, analyzer, IndexWriter.MaxFieldLength.UNLIMITED))
+            {
+                // remove older index entry
+                var searchQuery = new TermQuery(new Term("Id", record_id.ToString()));
+                writer.DeleteDocuments(searchQuery);
+
+                // close handles
+                analyzer.Close();
+                writer.Dispose();
+            }
+        }
+
+        public static bool ClearLuceneIndex()
+        {
+            try
+            {
+                var analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+                using (var writer = new IndexWriter(_directory, analyzer, true, IndexWriter.MaxFieldLength.UNLIMITED))
+                {
+                    // remove older index entries
+                    writer.DeleteAll();
+
+                    // close handles
+                    analyzer.Close();
+                    writer.Dispose();
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static void Optimize()
+        {
+            var analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+            using (var writer = new IndexWriter(_directory, analyzer, IndexWriter.MaxFieldLength.UNLIMITED))
+            {
+                analyzer.Close();
+                writer.Optimize();
+                writer.Dispose();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void InitFullIndex()
+        {
+            // add all existing records to Lucene search index
+            LuceneSearch.AddUpdateLuceneIndex(SampleDataRepository.GetAll());
+        }
+
+        static void IndexNewData(int _id, string _name, string _desc)
+        {
+            var new_record = new SampleData { Id = _id, Name = _name, Description = _desc };
+            // todo: add record to database...
+
+            // add record to Lucene search index
+            LuceneSearch.AddUpdateLuceneIndex(new_record);
+        }
+    }
+}
